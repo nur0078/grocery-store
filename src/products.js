1f405c19d30ebea4b33dcb94c05edcc79d1d6fe6
Anurodh <anurodhp7@gmail.com>
2023-07-18 18:21:01 +1000

    products Data

---
+++ src/products.js
@@ -0,0 +1,82 @@
+
+import Bananas from "./assets/products/Bananas.png"
+import BellPeppers from "./assets/products/BellPeppers.png"
+import BrownPotatoes from "./assets/products/BrownPotatoes.png"
+import Cucumbers from "./assets/products/Cucumbers.png"
+import GarlicCloves from "./assets/products/garlicCloves.avif"
+import Lettuce from "./assets/products/Lettuce.png"
+import MulaKoAchar from "./assets/products/MulaKoAchar.png"
+import RedApples from "./assets/products/RedApples.png"
+import RedOnions from "./assets/products/RedOnions.png"
+import TomatoPack from "./assets/products/TomatoPack.png"
+
+import YellowOnions from "./assets/products/YellowOnions.png"
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Red Onions",
+    price: 3.50,
+    productImage: RedOnions,
+  },
+  {
+    id: 2,
+    productName: "Yellow Onions",
+    price: 3.50,
+    productImage: YellowOnions,
+  },
+  {
+    id: 3,
+    productName: "Mula Ko Achar - Mitho Chha!",
+    price: 10.00,
+    productImage: MulaKoAchar,
+  },
+  {
+    id: 4,
+    productName: "Tomatoes",
+    price: 2.99,
+    productImage: TomatoPack,
+  },
+  {
+    id: 5,
+    productName: "Cucumbers",
+    price: 1.99,
+    productImage: Cucumbers,
+  },
+  {
+    id: 6,
+    productName: "Garlic Cloves",
+    price: 1.25,
+    productImage: GarlicCloves,
+  },
+  {
+    id: 7,
+    productName: "Lettuce",
+    price: 2.75,
+    productImage: Lettuce,
+  },
+  {
+    id: 8,
+    productName: "Red Apples",
+    price: 0.99,
+    productImage: RedApples,
+  },
+  {
+    id: 9,
+    productName: "Bananas",
+    price: 0.79,
+    productImage: Bananas,
+  },
+  {
+    id: 10,
+    productName: "Brown Potatoes",
+    price: 2.49,
+    productImage: BrownPotatoes,
+  },
+  {
+    id: 11,
+    productName: "Bell Peppers",
+    price: 1.99,
+    productImage: BellPeppers,
+  },
+];
