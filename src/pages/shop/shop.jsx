2834553aa6206de41747997d54f408a49039a784
Anurodh <anurodhp7@gmail.com>
2023-07-18 18:21:57 +1000

    shop component

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,19 @@
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
+import "./shop.css";
+
+export const Shop = () => {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>FreshPicks Groceries</h1>
+      </div>
+
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product data={product} key={product.id}/>
+        ))}
+      </div>
+    </div>
+  );
+};
