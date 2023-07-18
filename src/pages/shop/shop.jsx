5f2924a1a028e8ff9771d39f667baf01aa8fd455
Anurodh <anurodhp7@gmail.com>
2023-07-18 20:13:28 +1000

    added padding after shoptitle. before products

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -4,12 +4,12 @@ import "./shop.css";
 
 export const Shop = () => {
   return (
-    <div className="shop">
+    <div className="shop ">
       <div className="shopTitle">
-        <h1>FreshPicks Groceries</h1>
+        <h1 className="text-2xl pt-4">FreshPicks Groceries</h1>
       </div>
 
-      <div className="products">
+      <div className="products pt-10">
         {PRODUCTS.map((product) => (
           <Product data={product} key={product.id}/>
         ))}
